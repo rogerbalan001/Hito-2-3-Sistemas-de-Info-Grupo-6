b51d36682910e7ce5089815736087524e1989b24
Isolar03 <Isolar03@users.noreply.github.com>
2026-06-22 17:33:27 +0000

    deploy: 4e209a34f61613b526b4c7073e34361a61053d44

--- main.dart.js
+++ main.dart.js
@@ -30641,7 +30641,7 @@ $S:2}
 A.aEv.prototype={
 $1(a){var s=A.dE().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a4ce257c68517c1410f4b48ac9852ab5642a3f8d/":s)+a},
 $S:69}
 A.QB.prototype={
 gv(a){var s=this.a
